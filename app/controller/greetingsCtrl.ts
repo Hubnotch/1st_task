a42003b0401fef45e5ee483b2b9e018ad06235d2
Ekene Ugwuanyi <ekene.ugwuanyi@stanbicibtc.com>
2024-07-02 10:27:01 +0100

    fixes on installing Npm I ts-node-dev and clean up

--- app/controller/greetingsCtrl.ts
+++ app/controller/greetingsCtrl.ts
@@ -1,21 +1,25 @@
-import { Request,Response } from "express"
+import { Request, Response } from "express";
 import { getLocation, getTemperature } from "../utils/checkWeather";
 
-export const  greetingCtrl = async (request:Request, response:Response) =>{
-
+export const greetingCtrl = async (request: Request, response: Response) => {
   const visitorName = request.query.visitor_name || "Guest";
-const clientIp = request.headers["x-forwarded-for"] || request.socket.remoteAddress;
+  const clientIp =
+    request.headers["x-forwarded-for"] || request.socket.remoteAddress;
   const locationData = await getLocation(clientIp);
   if (!locationData || !locationData.city) {
-    return response.status(500).json({ error: "Unable to fetch location data" });
+    return response
+      .status(500)
+      .json({ error: "Unable to fetch location data" });
   }
-const temperature = await getTemperature(locationData.city);
-if (temperature === null) {
-  return response.status(500).json({ error: "Unable to fetch temperature data" });
-}
+  const temperature = await getTemperature(locationData.city);
+  if (temperature === null) {
+    return response
+      .status(500)
+      .json({ error: "Unable to fetch temperature data" });
+  }
   response.json({
     client_ip: clientIp,
     location: locationData.city,
     greeting: `Hello, ${visitorName}!, the temperature is ${temperature} degrees Celsius in ${locationData.city}`,
   });
-}
+};
