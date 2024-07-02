12481498dec429a2967cfcf11b376d2e9b4ca167
Ekene Ugwuanyi <ekene.ugwuanyi@stanbicibtc.com>
2024-07-02 10:55:39 +0100

    major updates

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express, { Express } from "express";
 import { publicRoute } from "./routes/publicRoutes";
 import dotenv from 'dotenv'
+import { greetingCtrl } from "./controller/greetingsCtrl";
 dotenv.config();
 
 export const app:Express = express();
 app.set("trust proxy", true);
 app.use(express.json());
-app.use('/api/hello',publicRoute)
+
+app.use("/api/hello", greetingCtrl);
+// app.use('/api/hello',publicRoute)
 const PORT = process.env.PORT || 2024;
 app.listen(PORT, () => console.log(`Server running on : localhost:${PORT}`));
